a00ab7c4fb6909bb5210ea762400dca159cb0eae
Kevin Tran <me@kevintran.codes>
2015-03-23 13:26:36 -0700

    oops

--- c++/LinkedList-template/singlylinkedlist.cpp
+++ c++/LinkedList-template/singlylinkedlist.cpp
@@ -53,7 +53,6 @@ T LinkedList<T>::get(int index) {
 
 template <typename T>
 T LinkedList<T>::remove(int index) {
-<<<<<<< HEAD
 	if (index < 0 || index >= length) {
 		throw LinkedListException(ARRAY_INDEX_OUT_OF_BOUNDS);
 	}
